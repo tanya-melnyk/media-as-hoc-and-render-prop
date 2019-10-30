0f78f89b3526b4f93999d741fc1fe93b10b85c1a
Tetiana Melnyk <prostotayna@gmail.com>
2019-10-30 10:11:25 +0200

    Updates

---
+++ precache-manifest.99bdd46992b42ecc7b872d2cb5890c46.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d3cb557d4634741ad7dc7ca1b774ee",
+    "url": "/media-as-hoc-and-render-prop/index.html"
+  },
+  {
+    "revision": "5f1f4ded47ea7cc0d32a",
+    "url": "/media-as-hoc-and-render-prop/static/js/2.f967dce1.chunk.js"
+  },
+  {
+    "revision": "9845025a7a3b4d301757",
+    "url": "/media-as-hoc-and-render-prop/static/js/main.28c6709e.chunk.js"
+  },
+  {
+    "revision": "d9d9357895e04430283f",
+    "url": "/media-as-hoc-and-render-prop/static/js/runtime-main.900bff6d.js"
+  }
+]);
